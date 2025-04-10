888b1d57d23ad29ebdf36493d19093a48e96a72e
AndreKauan <andre.kauan.antonelli1999@gmail.com>
2025-04-09 23:41:02 -0300

    Criado obj Scanner, variáveis e concatenando as informações em uma String

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite seu nome!");
+        String nome = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numConta = sc.nextInt();
+
+        System.out.println("Por favor, digite o Salda da conta!");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque");
+
+        sc.close();
     }
 }
