631480a7eed17c9fc4abaf66825d334e50fd333e
AndreKauan <andre.kauan.antonelli1999@gmail.com>
2025-04-23 23:05:02 -0300

    Criado méteodo Construtor e getSaldo

--- ContaBanco/src/ContaBancaria.java
+++ ContaBanco/src/ContaBancaria.java
@@ -3,5 +3,19 @@ public class ContaBancaria {
     private double saldo;
     private double chequeEspecial;
     
+    public ContaBancaria(double depositoInicial){
+        if (depositoInicial <= 500) {
+            chequeEspecial = 50;
+        }else{
+            chequeEspecial = depositoInicial * 0.5;
+        }
+        
+        saldo += depositoInicial+chequeEspecial;
+    }
+
+    // Consultar Saldo
+    public double getSaldo() {
+        return saldo;
+    }
 
 }
