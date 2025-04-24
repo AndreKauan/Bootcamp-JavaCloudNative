f74691416e8da9baae8b79b60b27d0326451fe8d
AndreKauan <andre.kauan.antonelli1999@gmail.com>
2025-04-23 22:47:18 -0300

    Preparação para aplicar POO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,19 +6,7 @@ public class ContaTerminal {
        
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         
-        System.out.println("Por favor, digite seu nome!");
-        String nome = sc.nextLine();
-
-        System.out.println("Por favor, digite o número da Agência!");
-        String agencia = sc.nextLine();
-
-        System.out.println("Por favor, digite o número da Conta!");
-        int numConta = sc.nextInt();
-
-        System.out.println("Por favor, digite o Salda da conta!");
-        double saldo = sc.nextDouble();
-
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque");
+        
 
         sc.close();
     }
