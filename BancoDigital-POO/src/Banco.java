1ec2e2bef89e00b3ef895a92b9d355f4ec403099
AndreKauan <andre.kauan.antonelli1999@gmail.com>
2025-05-10 11:30:53 -0300

    Atributos e Métodos - Classe Banco

--- BancoDigital-POO/src/Banco.java
+++ BancoDigital-POO/src/Banco.java
@@ -1,3 +1,24 @@
+import java.util.List;
+
 public class Banco {
+
+    private String instituicao;
+	private List<Conta> contas;
+
+	public String getInstituicao() {
+		return instituicao;
+	}
+
+	public void setNome(String instituicao) {
+		this.instituicao = instituicao;
+	}
+
+	public List<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(List<Conta> contas) {
+		this.contas = contas;
+	}
 
 }
